399eb9ca785ebbbe9471215df2a83f844425f0da
Herghys <helmi.santosa1@gmail.com>
2024-01-14 11:35:54 +0700

    Add project files.

---
+++ StardeValley-ModsList-NetFramework/DataModels/ManifestDataModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StardeValley_ModsList_NetFramework.DataModels
+{
+    public class ManifestDataModel
+    {
+        public ManifestDataModel() { }
+
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Version { get; set; }
+        public string Description { get; set; }
+        public string UniqueID { get; set; }
+        public string EntryDll { get; set; }
+        public string MinimumApiVersion { get; set; }
+        public List<string> UpdateKeys { get; set; }
+
+    }
+}
